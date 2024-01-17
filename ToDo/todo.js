485c979324aa4454d596a828e1599e6a9951b757
pavankumarlokare <pavanlokare1@gmail.com>
2024-01-18 00:57:14 +0530

    my first commit

---
+++ ToDo/todo.js
@@ -0,0 +1,48 @@
+const inputBox=document.getElementById("input-box");
+const listContainer=document.getElementById("List-container")
+function Addtask(){
+    if(inputBox.value===''){
+        alert("you must write something!")
+    }
+    else{
+        //creating html element of li
+        let li=document.createElement("li")
+        // passing that input values into that li
+        li.innerHTML=inputBox.value
+        //adding that html element where you exactly wants
+        listContainer.appendChild(li)
+        
+
+        // here i am creating one span element directly in js then adding delete image code into that,then appending/adding where u wants to
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span)
+    }
+     // //after adding inputbox should be empty
+    inputBox.value='';
+    saveData();
+}
+   
+
+    listContainer.addEventListener("click",function(e){
+        if(e.target.tagName==="LI"){
+            e.target.classList.toggle("checked")
+            saveData()
+        }
+      
+        else if(e.target.tagName==="SPAN"){
+            e.target.parentElement.remove()
+            saveData()
+        }
+    },false)
+
+    function saveData(){
+        localStorage.setItem("data", listContainer.innerHTML);
+        
+    }
+    function showTask(){
+        listContainer.innerHTML=localStorage.getItem("data");
+    }
+    showTask();
+
+
